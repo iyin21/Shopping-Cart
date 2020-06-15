35e20c917730fe5d931ec36e7c2c475ce6d94b2a
Iyin Oluleke <iyinoluwa.olulek@gmail.com>
2020-06-15 18:26:00 +0100

    Add partials and index page

--- app.js
+++ app.js
@@ -8,18 +8,36 @@ var logger = require("morgan");
 //require routes
 var indexRoutes = require("./routes/index");
 //view engine setup
-//app.engine("hbs", hbs({defaultLayout: "layout"}));
+app.engine("hbs", hbs({defaultLayout: "layout", extname:".hbs"}));
 //app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
+app.set("view engine", ".hbs");
 //bodyparser setup
 app.use(bodyParser.urlencoded({extended : true}));
 //setup morgan
 app.use(logger("dev"));
-
+//public directory
+app.use(express.static(__dirname +"/public"));
 
 
 app.use("/", indexRoutes);
 
 app.listen("3000", function(){
 	console.log("App running");
-})
+});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next){
+// 	var err = new Error("Not found");
+// 	err.status = 404;
+// 	next(err);
+// });
+// //error handlers
+// //development error handler
+// if(app.get("env")==="development"){
+// 	app.use(function(err, req, res, next){
+// 		res.status(err.status || 500);
+// 		res.render("error", {
+// 			message: err.message,
+// 			error: err
+// 		});
+// 	});
+// }
